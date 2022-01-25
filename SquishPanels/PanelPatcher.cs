1cd6ddb9e54d096047f1fe599bb884ea2e45c183
Cyro <BlueCyro@protonmail.com>
2022-01-25 10:58:02 -0600

    Fixed issue where some interfaces (such as DevCreateNewForm and FullBodyCalibrator) were scaled incorrectly. Also changed from scaling the entire slot to just scaling the Content slot (where the Canvas component lives) since NeosPanel handles scaling anyways.

--- SquishPanels/PanelPatcher.cs
+++ SquishPanels/PanelPatcher.cs
@@ -7,6 +7,7 @@ using NeosModLoader;
 using HarmonyLib;
 using FrooxEngine;
 using BaseX;
+using FrooxEngine.UIX;
 
 namespace SquishPanels
 {
@@ -23,27 +24,33 @@ namespace SquishPanels
         }
 
 
-        [HarmonyPatch(typeof(NeosPanel),"OnAttach")]
-        class SceneInspectorPatcher
+        [HarmonyPatch(typeof(NeosPanel),"OnChanges")]
+        class NeosPanelOnChangePatcher
         {
             static void Postfix(NeosPanel __instance)
             {
-                float3 LocalScl = __instance.Slot.LocalScale;
-                __instance.Slot.Scale_Field.TweenFromTo(new float3(LocalScl.x, 0f, LocalScl.z), LocalScl, 0.22f);
-                StaticAudioClip clip = __instance.Slot.AttachAudioClip(new Uri("neosdb:///bbdf36b8f036a5c30f7019d68c1fbdd4032bb1d4c9403bcb926bb21cd0ca3c1a.wav"));
-                AudioOutput audio = __instance.World.PlayOneShot(__instance.Slot.GlobalPosition, clip, 1f, true, 1f, __instance.Slot, AudioDistanceSpace.Local, false);
-                audio.DopplerLevel.Value = 0f;
-                audio.DistanceSpace.Value = AudioDistanceSpace.Global;
+                if (__instance.WhiteList.Count > 0 && __instance.WhiteList.Count < 2)
+                {
+                    Slot ContentSlot = __instance.WhiteList[0].Slot;
+                    float3 LocalScl = ContentSlot.LocalScale;
+                    ContentSlot.Scale_Field.TweenFromTo(new float3(LocalScl.x, 0f, LocalScl.z), LocalScl, 0.22f);
+                    StaticAudioClip clip = __instance.Slot.AttachAudioClip(new Uri("neosdb:///bbdf36b8f036a5c30f7019d68c1fbdd4032bb1d4c9403bcb926bb21cd0ca3c1a.wav"));
+                    AudioOutput audio = __instance.World.PlayOneShot(__instance.Slot.GlobalPosition, clip, 1f, true, 1f, __instance.Slot, AudioDistanceSpace.Local, false);
+                    audio.DopplerLevel.Value = 0f;
+                    audio.DistanceSpace.Value = AudioDistanceSpace.Global;
+                    __instance.WhiteList.Add(null);
+                }
             }
         }
 
         [HarmonyPatch(typeof(NeosPanel),"OnClose")]
-        class NeosPanelPatcher
+        class NeosPanelOnClosePatcher
         {
             static bool Prefix(NeosPanel.TitleButton button, NeosPanel __instance)
             {
-                float3 LocalScl = __instance.Slot.LocalScale;
-                __instance.Slot.Scale_Field.TweenFromTo(LocalScl, new float3(LocalScl.x, 0f, LocalScl.z), 0.22f, CurvePreset.Sine, null, new Action(__instance.Slot.Destroy));
+                Slot ContentSlot = (AccessTools.Field(typeof(NeosPanel), "_contentSlot").GetValue(__instance) as SyncRef<Slot>).Target;
+                float3 LocalScl = ContentSlot.LocalScale;
+                ContentSlot.Scale_Field.TweenFromTo(LocalScl, new float3(LocalScl.x, 0f, LocalScl.z), 0.22f, CurvePreset.Sine, null, new Action(__instance.Slot.Destroy));
                 StaticAudioClip clip = __instance.Slot.AttachAudioClip(new Uri("neosdb:///e600ed8a6895325613b82a50fd2a8ea2ac64151adc5c48c913d33d584fdf75d5.wav"));
                 AudioOutput audio = __instance.World.PlayOneShot(__instance.Slot.GlobalPosition, clip, 1f, true, 1f, __instance.Slot, AudioDistanceSpace.Local, false);
                 audio.DopplerLevel.Value = 0f;
