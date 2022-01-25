a22c10aaa896fb3da39d02e7f80f69ad45357053
Cyro <BlueCyro@protonmail.com>
2022-01-25 10:59:20 -0600

    Remove unused libraries

--- SquishPanels/PanelPatcher.cs
+++ SquishPanels/PanelPatcher.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NeosModLoader;
 using HarmonyLib;
 using FrooxEngine;
 using BaseX;
-using FrooxEngine.UIX;
 
 namespace SquishPanels
 {
